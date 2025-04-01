3ddd063a7d244217647f2d8ae6b5488dab680ca0
tania sharma <bishupandit07@gmail.com>
2025-04-01 18:52:22 +0530

    images

--- weather-widget/src/App.js
+++ weather-widget/src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
-import InfoBox from './InfoBox.jsx';
-import SearchBox from './SearchBox.jsx';
 import WeatherApp from './WeatherApp.jsx';
+
 function App() {
   return (
     <>
     <WeatherApp/>
-     {/* <SearchBox/>
-     <InfoBox/> */}
+    
     </>
   );
 }
